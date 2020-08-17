72af1dbe2f1827f40dacbd6d26f29d6d45571028
jzembrzuski <jzembrzuski@gmail.com>
2020-08-17 19:46:36 -0400

    Majority of angular work

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,9 +19,7 @@ class About extends Component {
     return (
       <section id="about">
       <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Jake Profile Pic" />
-         </div>
+         
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
